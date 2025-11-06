972ad4cb5b362b21914adc1c00646347df9b1823
Muhammed Hüseyin Özkaya <176496712+mhuseyinozkaya@users.noreply.github.com>
2025-11-07 01:47:14 +0300

    More functionality added

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 check_root(){
-    if [ "$(id -u)" -ne 0 ]; then
+    if [ "$(id -u)" -ne 0 ];then
         echo "[!] Script requires privilege permission (use sudo)"
         exit 1
     fi
@@ -9,7 +9,9 @@ check_root(){
 
 check_root
 
-DEFAULT_WORKDIR="/usr/local/lib/tunsel"
+SCRIPT_NAME="tunsel"
+
+DEFAULT_WORKDIR="/usr/local/etc/$SCRIPT_NAME"
 mkdir -p "$DEFAULT_WORKDIR"
 
 WG_DIR="$DEFAULT_WORKDIR/wireguard"
@@ -24,62 +26,100 @@ OVPN_PID="$DEFAULT_WORKDIR/active_ovpn.pid"
 ARGS=("$@")
 ARGS_COUNT="$#"
 
+info(){
+    echo "$SCRIPT_NAME - Tunnel selector for Wireguard and OpenVPN protocols"
+}
+
 #   Argument parsing
 argument_parser(){
-    if [[ "$ARGS_COUNT" -eq 1 && "${ARGS[$ARGS_COUNT-1]}" == "wconnect" ]];then
+    if [[ "$ARGS_COUNT" -eq 1 && "${ARGS[0]}" == "wconnect" ]];then
         connect_tunnel "$WG_DIR"
-    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[$ARGS_COUNT-1]}" == "oconnect" ]];then
+    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[0]}" == "oconnect" ]];then
         connect_tunnel "$OVPN_DIR"
-    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[$ARGS_COUNT-1]}" == "disconnect" ]];then
+    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[0]}" == "disconnect" ]];then
         disconnect_tunnel
-    elif [[ "$ARGS_COUNT" -eq 2 && "${ARGS[0]}" == "import" && -s "$(realpath "${ARGS[1]}")" ]];then
-        import_tunnel_file "${ARGS[1]}"
+    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[0]}" == "status" ]];then
+        connection_status
+    elif [[ "$ARGS_COUNT" -ge 2 && "${ARGS[0]}" == "import" ]];then
+        import_tunnel_file "${ARGS[@]:1}"    
+    elif [[ "$ARGS_COUNT" -eq 1 && "${ARGS[0]}" == "list" ]];then
+        list_tunnels
+    elif [[ "$ARGS_COUNT" -eq 0 ]];then
+        info
+    else
+        echo "[!] Unknown usage, for help use $0 help"
     fi
 }
 
-import_tunnel_file(){
-    local FILE_NAME="$1"
-    local FILE_REALPATH="$(realpath "$FILE_NAME" 2>/dev/null)"
-
-    if [[ -f "$FILE_REALPATH" ]];then
-        if [[ "$FILE_REALPATH" == *.conf ]];then
-            cp "$FILE_REALPATH" "$WG_DIR"
-            echo "[+] Tunnel file successfully imported"
-            exit
-        elif [[ "$FILE_REALPATH" == *.ovpn ]];then
-            cp "$FILE_REALPATH" "$OVPN_DIR"
-            echo "[+] Tunnel file successfully imported"
-            exit
-        else
-            echo "Unknown file type, only .ovpn and .conf are supported"
-            exit
-        fi
+connection_status(){
+    local up_epoch current_epoch connection_epoch formatted_up_time interface
+    if [[ -s "$WG_STATE" ]];then
+        connection_epoch=$(stat -c %Y "$WG_STATE")
+        interface="Wireguard: $(< $WG_STATE)"
+    elif [[ -s "$OVPN_PID" ]];then
+        connection_epoch=$(stat -c %Y "$OVPN_PID")
+        interface="OpenVPN PID: $(< $OVPN_PID)"
     else
-        echo "[!] Import failed"
-        exit
+        echo "[*] Status: Disconnected"
+        return 
     fi
+    current_epoch=$(date +%s)
+    up_epoch=$((current_epoch - connection_epoch))
+    formatted_up_time=$(date -u -d "@$up_epoch" +'%H:%M:%S')
+    echo "[*] Status: Connected, Uptime: $formatted_up_time"
+    echo "[*] Tunnel: $interface"
+    return 0
+}
+
+import_tunnel_file(){
+    local FILES=("$@")
+    local file_realpath
+    for file in "${FILES[@]}";do
+        file_realpath="$(realpath "$file" 2>/dev/null)"
+        if [[ -s "$file_realpath" ]];then
+            if [[ "$file_realpath" == *.conf ]];then
+                cp "$file_realpath" "$WG_DIR"
+                echo "[+] Wireguard tunnel $(basename "$file_realpath") successfully imported"
+            elif [[ "$file_realpath" == *.ovpn ]];then
+                cp "$file_realpath" "$OVPN_DIR"
+                echo "[+] OpenVPN tunnel $(basename "$file_realpath") successfully imported"
+            else
+                echo "[!] Unknown file type for $(basename "$file_realpath"), only .ovpn and .conf supported"
+            fi
+        else
+            echo "[!] Import failed, file $(basename "$file_realpath") not found or empty"
+        fi
+    done
 }
 
 list_tunnels(){
     local DIR_NAME="$1"
+    local message="Available all profiles:"
     shopt -s nullglob
     # Create global tunnels list
     if [[ "$DIR_NAME" == "$WG_DIR" ]];then
         tunnels=("$DIR_NAME"/*.conf)
-    else
+        message="Wireguard profiles:"
+    elif [[ "$DIR_NAME" == "$OVPN_DIR" ]];then
         tunnels=("$DIR_NAME"/*.ovpn)
+        message="OpenVPN profiles:"
+    else
+        tunnels=("$WG_DIR"/*.conf "$OVPN_DIR"/*.ovpn)        
     fi
     shopt -u nullglob
 
     if [[ ${#tunnels[@]} -eq 0 ]];then
         echo "[!] No tunnel file found to connect"
-        exit
+        exit 1
     fi
 
-    echo "Available tunnel profiles:"
+    # Print messages
+    echo "$message"
+
     for i in "${!tunnels[@]}"; do
         echo "$((i + 1)). $(basename "${tunnels[i]}")"
     done
+    return 0
 }
 
 connect_tunnel(){
@@ -93,15 +133,24 @@ connect_tunnel(){
     list_tunnels "$software"
     read -rp "[*] Please specify a tunnel to connect: " value
 
+    if ! [[ "$value" =~ ^[0-9]+$ ]];then
+        echo "[!] Invalid input: Please enter a number."
+        return 1
+    fi
+    
+    if [[ "$value" -lt 1 || "$value" -gt ${#tunnels[@]} ]];then
+        echo "[!] Invalid selection: Number out of range."
+        return 1
+    fi
+    
     if [[ "$software" == "$WG_DIR" ]];then
-        if wg-quick up "${tunnels[$value-1]}" 1>/dev/null 2>&1; then
+        if wg-quick up "${tunnels[$value-1]}" 1>/dev/null 2>&1;then
             echo "${tunnels[$value-1]}" > "$WG_STATE"
-            echo "[+] Successfully connected to $(basename "${tunnels[$value-1]}") tunnel."
+            echo "[+] Successfully connected to Wireguard $(basename "${tunnels[$value-1]}") tunnel."
         fi
     elif [[ "$software" == "$OVPN_DIR" ]];then
-        local ovpn_pid=""
         if openvpn --config "${tunnels[$value-1]}" --daemon --writepid "$OVPN_PID";then
-            echo "[+] Successfully connected to $(basename "${tunnels[$value-1]}") tunnel."
+            echo "[+] Successfully connected to OpenVPN tunnel with PID $(basename "${tunnels[$value-1]}")"
         fi
     fi
 }
@@ -117,7 +166,7 @@ disconnect_tunnel(){
         local pid="$(< "$OVPN_PID")"
         if kill "$pid" 1>/dev/null;then
             rm -f "$OVPN_PID"
-            echo "[-] You successfully disconnected from OpenVPN interface."
+            echo "[-] You successfully disconnected from OpenVPN interface which PID $pid."
         fi
     else
         echo "[!] You already disconnected."
